a888238c2b2590f72023bde0978ef37f53fc4b5e
SajidaDahir <sajidadahirsheikh@gmail.com>
2025-05-05 21:12:41 +0300

    Using Scanner to input data in terminal

---
+++ Challenge4/inputExample.java
@@ -0,0 +1,25 @@
+package Challenge4;
+
+
+import java.util.Scanner;
+
+public class inputExample {
+    Scanner scanner = new Scanner(System.in);
+
+    public void readNameAndAge() {
+        String name;
+        int age;
+        System.out.println("Name? ");
+        name= scanner.next();
+
+        System.out.println("Age? ");
+        age=scanner.nextInt();
+
+        System.out.println("Hello " +name+", you are "+age+" springs young");
+
+    }
+    public static void main(String[] args){
+        (new inputExample()).readNameAndAge();
+    }
+
+}
